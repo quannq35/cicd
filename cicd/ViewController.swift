b4c495149c96a1fd65bddef88d6d37c71bad00b4
Quân Nguyễn <>
2024-07-27 10:35:37 +0700

    quann commit

--- cicd/ViewController.swift
+++ cicd/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         Do any additional setup after loading the view.
 //        print("URL is", CicdEnviroment.rootURL)
 //        print("API_Key is", CicdEnviroment.apiKey)
 //        
